95b2841e74f81f71cdcad349fd9d19a8bbbacc5e
Sam <Sam@192.168.1.63>
2022-08-01 19:06:38 -0400

    implemented user story 123 automation script

--- src/demo/DemoTest.java
+++ src/demo/DemoTest.java
@@ -4,5 +4,9 @@ public class DemoTest {
 	public static void main(String[] args) {
 		System.out.println("test");
 	}
+	// for user story 123
+	public static void add(int a, int b) {
+		System.out.println(a+b);
+	}
 
 }
